c54f4c013d8e375546f0a553953158bb32943768
Anartz Mugika <mugan86@gmail.com>
2017-03-21 07:17:50 +0100

    Add detail component

---
+++ src/pages/page3/page3.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page2',
+  templateUrl: 'page3.html'
+})
+export class Page3 {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+  }
+}
