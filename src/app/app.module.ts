69743dd5be0bdc17f695687fad6efb33f7aa0a00
Anartz Mugika <mugan86@gmail.com>
2017-03-25 08:05:29 +0100

    Import directive after create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+//Pages
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { Page3 } from '../pages/page3/page3';
+
+//Directives
+import { DefaultImage } from '../directives/default-image';
+
+//Services
 import { ConvertedService } from "./../services/converter";
 
 @NgModule({
