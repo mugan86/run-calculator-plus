ff9e21df5f6ccad84a7561d80e4cc4a519609f13
Anartz Mugika <mugan86@gmail.com>
2017-03-25 08:04:50 +0100

    Add directives directory with DefaultImage Directive to load default image if not find define url

---
+++ src/directives/default-image.ts
@@ -0,0 +1,22 @@
+//our root app component
+import {Component, Directive, Output, EventEmitter, Input, SimpleChange} from '@angular/core'
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import 'rxjs/Rx';
+
+@Directive({
+  selector: 'img[default]',
+  host: {
+    '(error)':'updateUrl()',
+    '[src]':'src'
+   }
+})
+export class DefaultImage {
+  @Input() src:string;
+  //Add default image to show if not load correct select src
+  @Input() default:string = "./assets/img/icons/birthday.png";
+
+  updateUrl() {
+    this.src = this.default;
+  }
+}
