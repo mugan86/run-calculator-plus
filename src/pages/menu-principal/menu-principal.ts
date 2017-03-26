b5a29e1eccd847b6cf2ef8c4d261c3ec1de59368
Anartz Mugika <mugan86@gmail.com>
2017-03-26 18:47:15 +0200

    Remove unused Translation import

--- src/pages/menu-principal/menu-principal.ts
+++ src/pages/menu-principal/menu-principal.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { RunConverter } from '../../models/run-converter';
+import { TranslateService } from 'ng2-translate';
 
 @Component({
   selector: 'page-menu-principal',
@@ -10,8 +11,9 @@ import { RunConverter } from '../../models/run-converter';
 export class MenuPrincipal {
   converter: RunConverter;
   convertions = [{icon: 'iii', label: 'Kms'}];
-  constructor(public navCtrl: NavController) {
-     this.converter= new RunConverter();
+  constructor(public navCtrl: NavController, public translate: TranslateService) {
+    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
+    this.converter= new RunConverter();
     console.log("17.9 km/h = " + this.converter.KilometersPerHourToPaceMinKm(17.9)  + " min/km");
     console.log(this.convertions[0].icon);
   }
