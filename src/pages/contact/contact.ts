70a6281f8623374bee6f767c556577b02c41a390
Anartz Mugika <mugan86@gmail.com>
2017-03-28 07:43:23 +0200

    Add contact page

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { RunConverter } from '../../models/run-converter';
+import { TranslateService } from 'ng2-translate';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class Contact {
+  converter: RunConverter;
+  convertions = [{icon: 'iii', label: 'Kms'}];
+  constructor(public navCtrl: NavController, public translate: TranslateService) {
+    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
+    this.converter= new RunConverter();
+    console.log("17.9 km/h = " + this.converter.KilometersPerHourToPaceMinKm(17.9)  + " min/km");
+    console.log(this.convertions[0].icon);
+  }
+
+  makeBasicConversion()
+  {
+    console.log("21.1 km/h = " + this.converter.KilometersPerHourToPaceMinKm(21.1)  + " min/km");
+  }
+}
