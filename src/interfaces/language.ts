134c99531ce74ff9552d43d3b61df66d7f91dc14
Anartz Mugika <mugan86@gmail.com>
2017-04-03 07:20:54 +0200

    Add language documentation

--- src/interfaces/language.ts
+++ src/interfaces/language.ts
@@ -2,6 +2,8 @@
  * Interface to manage different conversions
  */
 export interface ILanguage{
+    //Language code, for example 'eu' in basque language
     code: string;
+    //Show language text label. For exmple in basque show "Euskara"
     label: string;
 }
