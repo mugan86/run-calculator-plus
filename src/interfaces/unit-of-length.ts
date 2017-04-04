c16715ed4d95b698e2ea6546c01295b781354778
Anartz Mugika <mugan86@gmail.com>
2017-04-04 08:20:40 +0200

    Add UnitOfLength Interface to manage differents unit systems (m or mille)

---
+++ src/interfaces/unit-of-length.ts
@@ -0,0 +1,9 @@
+/**
+ * Interface to manage different unit of lenghts
+ */
+export interface IUnitOfLength{
+    //unit of lenght code, for example 'm' in metric
+    code: string;
+    //Show unit of length text label. For exmple in metric show show "Metric"
+    label: string;
+}
