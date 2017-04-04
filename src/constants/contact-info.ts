9b1b32dd38fe7983f443cbe15892bba47b4df16c
Anartz Mugika <mugan86@gmail.com>
2017-04-04 09:41:57 +0200

    Contact info constants

---
+++ src/constants/contact-info.ts
@@ -0,0 +1,10 @@
+export const email = "mugan86@gmail.com";
+export const twitter = {
+    "userID": "@mugan86",
+    "url": ""
+};
+export const github = {
+    "userID": "mugan86",
+    "url": ""
+}
+    
