54e2b445a180ae4357c94f3d589f50e7a93e36e8
Anartz Mugika <mugan86@gmail.com>
2017-04-04 09:45:50 +0200

    Update with google play data

--- src/constants/contact-info.ts
+++ src/constants/contact-info.ts
@@ -6,5 +6,11 @@ export const twitter = {
 export const github = {
     "userID": "mugan86",
     "url": ""
-}
+};
+
+export const googlePlay ={
+    "appsUrlPrincipal": "https://play.google.com/store/apps/",
+    "myApps": "developer?id=Anartz+Muxika",
+    "serviraceApp": "details?id=com.amugika.servirace"
+};
     
