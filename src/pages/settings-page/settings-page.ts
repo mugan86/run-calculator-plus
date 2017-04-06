678653b79851585c792e35a1465a35d337a5ee30
Anartz Mugika <mugan86@gmail.com>
2017-04-06 13:50:11 +0200

    Load languageSelections constant from config.ts in constants

--- src/pages/settings-page/settings-page.ts
+++ src/pages/settings-page/settings-page.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TranslateService } from 'ng2-translate';
+
+//Models / Interfaces
 import { RunConverter } from './../../models/run-converter';
 import { ILanguage } from './../../interfaces/language';
 import { IUnitOfLength } from './../../interfaces/unit-of-length';
-/*
-  Generated class for the SettingsPage page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+//Constants
+import { languagesSelections } from './../../constants/config';
+
 @Component({
   selector: 'page-settings-page',
   templateUrl: 'settings-page.html'
@@ -37,19 +37,7 @@ export class SettingsPage {
 
   initializeValues() {
     this.language = localStorage.getItem('selectLanguage');
-    this.languages = [
-      { label: 'LANGUAGE.LABELS.CATALA', code: 'ca'},
-      { label: 'LANGUAGE.LABELS.DEUTCH', code: 'de'},
-      { label: 'LANGUAGE.LABELS.ENGLISH', code: 'en'},
-      { label: 'LANGUAGE.LABELS.SPANISH', code: 'es'},
-      { label: 'LANGUAGE.LABELS.BASQUE', code: 'eu'},
-      { label: 'LANGUAGE.LABELS.FRANCAIS', code: 'fr'},
-      { label: 'LANGUAGE.LABELS.GALEGO', code: 'gl'},
-      { label: 'LANGUAGE.LABELS.ITALIANO', code: 'it'},
-      { label: 'LANGUAGE.LABELS.JAPANESE', code: 'ja'},
-      { label: 'LANGUAGE.LABELS.PORTUGUES', code: 'pt'},
-      { label: 'LANGUAGE.LABELS.RUSO', code: 'ru'}
-    ];
+    this.languages = languagesSelections;
 
     this.unitLength = localStorage.getItem('unitOfLengthSelect');
 
