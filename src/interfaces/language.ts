cc7fde6fa060c938dc5ff6918b1aa1218585897f
Anartz Mugika <mugan86@gmail.com>
2017-04-06 07:06:23 +0200

    Replace interface with class

--- src/interfaces/language.ts
+++ src/interfaces/language.ts
@@ -1,7 +1,7 @@
 /**
  * Interface to manage different conversions
  */
-export interface ILanguage{
+export class ILanguage{
     //Language code, for example 'eu' in basque language
     code: string;
     //Show language text label. For exmple in basque show "Euskara"
