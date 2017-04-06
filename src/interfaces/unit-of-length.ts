3f8d1d6704a476c4181abff22d7dfd8d05c86ffe
Anartz Mugika <mugan86@gmail.com>
2017-04-06 07:06:41 +0200

    Replace interface with class

--- src/interfaces/unit-of-length.ts
+++ src/interfaces/unit-of-length.ts
@@ -1,7 +1,7 @@
 /**
  * Interface to manage different unit of lenghts
  */
-export interface IUnitOfLength{
+export class IUnitOfLength{
     //unit of lenght code, for example 'm' in metric
     code: string;
     //Show unit of length text label. For exmple in metric show show "Metric"
