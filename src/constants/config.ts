922a3606095193452e5d2d459ec9bc54c6fb9bac
Anartz Mugika <mugan86@gmail.com>
2017-04-06 06:38:18 +0200

    Add all languages strings and codes in one constant value

---
+++ src/constants/config.ts
@@ -0,0 +1,15 @@
+export const languagesSelections = [
+      { label: 'Catalá', code: 'ca'},
+      { label: 'Deutsch', code: 'de'},
+      { label: 'English', code: 'en'},
+      { label: 'Español', code: 'es'},
+      { label: 'Euskara', code: 'eu'},
+      { label: 'Français', code: 'fr'},
+      { label: 'Galego', code: 'gl'},
+      { label: 'Italiano', code: 'it'},
+      { label: 'Japanese', code: 'ja'},
+      { label: 'Português', code: 'pt'},
+      { label: 'Pусский', code: 'ru'}
+    ];
+
+  
