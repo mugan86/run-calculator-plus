e581c53a3632f54390f94beb88b0fe2c6b9a2e74
Anartz Mugika <mugan86@gmail.com>
2017-04-06 07:14:00 +0200

    Add id property to asign after select one theme

--- src/interfaces/theme.ts
+++ src/interfaces/theme.ts
@@ -1,7 +1,9 @@
 /**
- * Interface to manage different slides
+ * Interface to manage different themes
  */
 export interface ITheme{
+    //Theme id
+    id: string;
     //Theme main color code
     code: string;
     //Show theme name.
