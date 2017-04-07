c5040ec50ac15cad0749431aa61ee7d12c136fd4
Anartz Mugika <mugan86@gmail.com>
2017-04-07 07:28:31 +0200

    Start to create select component with different options

--- src/app/components/select-options/select-options.ts
+++ src/app/components/select-options/select-options.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { TranslateService } from 'ng2-translate';
+
+import { ILanguage } from './../../../interfaces/language';
+import { ITheme } from './../../../interfaces/theme';
+import { ISettings } from './../../../interfaces/settings';
 
 @Component({
   selector: 'select-options',
   templateUrl: 'select-options.html'
 })
 export class SelectOption {
+  @Input() minValue: number = 0;
+  @Input() maxValue: number = 59;
+  //Manage select option type
+  @Input() type: string;
+  @Input() languagesList: ILanguage[] = [];
   convertions = [{icon: 'iii', label: 'Kms'}];
   selectColor = "twitter";
   constructor(public navCtrl: NavController, public translate: TranslateService) {
