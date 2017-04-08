8fa8f388646e4117131e944ed2bf7c6e732e6d02
Anartz Mugika <mugan86@gmail.com>
2017-04-08 18:40:15 +0200

    Remove unused element and imports

--- src/app/components/select-options/select-options.ts
+++ src/app/components/select-options/select-options.ts
@@ -1,8 +1,5 @@
 import { Component, Input } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-import { TranslateService } from 'ng2-translate';
-
 import { ILanguage } from './../../../interfaces/language';
 import { ITheme } from './../../../interfaces/theme';
 import { ISettings } from './../../../interfaces/settings';
@@ -14,14 +11,12 @@ import { ISettings } from './../../../interfaces/settings';
 export class SelectOption {
   @Input() minValue: number = 0;
   @Input() maxValue: number = 59;
-  //Manage select option type
+  //Manage select option type (hour, min, second, )
   @Input() type: string;
-  @Input() languagesList: ILanguage[] = [];
-  convertions = [{icon: 'iii', label: 'Kms'}];
+
   selectColor = "twitter";
-  constructor(public navCtrl: NavController, public translate: TranslateService) {
-    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
-    console.log(this.convertions[0].icon);
+  constructor() {
+    
   }
 
 }
