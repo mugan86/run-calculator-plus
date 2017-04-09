396570c00003040797e22b96071a1a6b056d0688
Anartz Mugika <mugan86@gmail.com>
2017-04-09 17:49:37 +0200

    Remove unused imports

--- src/pages/menu-principal/menu-principal.ts
+++ src/pages/menu-principal/menu-principal.ts
@@ -11,13 +11,9 @@ import { SettingsService } from "./../../services/settings";
   templateUrl: 'menu-principal.html'
 })
 export class MenuPrincipal {
-  converter: RunConverter;
-  convertions = [{icon: 'iii', label: 'Kms'}];
   selectColor = "lightseagreen";
   constructor(public navCtrl: NavController, public translate: TranslateService, 
               private menuCtrl: MenuController, private settings: SettingsService) {
-
-
     //Active menu
     this.menuCtrl.enable(true);
 
@@ -30,23 +26,12 @@ export class MenuPrincipal {
     {
       console.log("Update preferences!!");
       settings.ourPreferences.welcomeComplete = true;
-      
       settings.updatePreferences(settings.ourPreferences);
       //Finish to view welcome page and reload only this time
       window.location.reload();
     }
-
-
     translate.setDefaultLang(localStorage.getItem('selectLanguage'));
-    this.converter= new RunConverter();
-    console.log("17.9 km/h = " + this.converter.KilometersPerHourToPaceMinKm(17.9)  + " min/km");
-    console.log(this.convertions[0].icon);
+   
   }
-
-  makeBasicConversion()
-  {
-    console.log("21.1 km/h = " + this.converter.KilometersPerHourToPaceMinKm(21.1)  + " min/km");
-  }
-
 
 }
