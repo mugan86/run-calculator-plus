c0dad278252ddfceafc2d03d9be3c1dfd57cce9d
Anartz Mugika <mugan86@gmail.com>
2017-04-10 20:10:14 +0200

    Optimize getters of settings values to return always values

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -2,6 +2,7 @@ import { ISettings } from './../interfaces/settings';
 import { ITheme } from './../interfaces/theme';
 import { ILanguage } from './../interfaces/language';
 import { localStorageValues } from './../constants/local-storage';
+import { themesListSelection } from './../constants/config';
 
 export class SettingsService {
 
@@ -23,6 +24,8 @@ export class SettingsService {
             this.ourPreferences = JSON.parse(localStorage.getItem(localStorageValues['userPreferences']));
         }
         else{
+            this.defaultLanguage = "es";
+            this.themeSelect = themesListSelection[3];
             this.ourPreferences = {
                             "langCode": this.defaultLanguage, 
                             "defaultTheme": this.themeSelect,
@@ -49,16 +52,19 @@ export class SettingsService {
 
     getTheme()
     {
+        if (this.ourPreferences.defaultTheme == null || this.ourPreferences.defaultTheme === undefined) return themesListSelection[3];
         return this.ourPreferences.defaultTheme;
     }
 
     getSelectLanguage()
     {
+        if (this.ourPreferences.langCode === null || this.ourPreferences.langCode === undefined || this.ourPreferences.langCode === "") return "es";
         return this.ourPreferences.langCode;
     }
 
     getSelectUnitLength()
     {
+        if (this.ourPreferences.unitOfLength === null || this.ourPreferences.unitOfLength === undefined || this.ourPreferences.unitOfLength === "") return "km";
         return this.ourPreferences.unitOfLength;
     }
 
