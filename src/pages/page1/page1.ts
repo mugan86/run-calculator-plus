e0f784c8244c837e6c0ab80514f8565bba59a628
Anartz Mugika <mugan86@gmail.com>
2017-04-14 17:19:46 +0200

    Load preferences value and load with select language and theme

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,32 +1,20 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-import { RunConverter } from './../../models/run-converter';
 import { TranslateService } from 'ng2-translate';
+
+import { SettingsService } from "./../../services/settings";
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
-  converter: RunConverter;
+  
   selectColor = "twitter";
-  constructor(public navCtrl: NavController, public translate: TranslateService) {
-    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
-    this.converter= new RunConverter();
-    console.log("17.9 km/h = " + this.converter.KilometersPerHourToPaceMinKm(17.9)  + " min/km");
+  constructor(public navCtrl: NavController, public translate: TranslateService, private settings: SettingsService,) {
+    translate.setDefaultLang(settings.getSelectLanguage());
+    
   }
 
-  makeBasicConversion()
-  {
-    console.log("21.1 km/h = " + this.converter.KilometersPerHourToPaceMinKm(21.1)  + " min/km");
-  }
-
-  changeLanguage (newLanguage: string) {
-		this.translate.use(newLanguage).subscribe(ready => {
-			//this.updateVariables();
-      console.log("Select language is: " + newLanguage);
-      localStorage.setItem('selectLanguage', newLanguage);
-		});
-	}
 }
