d9d5cfbfa824d36a2fa62e8acc3f0c9705d79e3f
Anartz Mugika <mugan86@gmail.com>
2017-04-14 07:05:39 +0200

    Add available value in themes and languages to manage in layouts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,24 +1,24 @@
 export const languagesSelections = [
-      { label: 'Catalá', code: 'ca'},
-      { label: 'Deutsch', code: 'de'},
-      { label: 'English', code: 'en'},
-      { label: 'Español', code: 'es'},
-      { label: 'Euskara', code: 'eu'},
-      { label: 'Français', code: 'fr'},
-      { label: 'Galego', code: 'gl'},
-      { label: 'Italiano', code: 'it'},
-      { label: 'Japanese', code: 'ja'},
-      { label: 'Português', code: 'pt'},
-      { label: 'Pусский', code: 'ru'}
+      { label: 'Catalá', code: 'ca', available: false},
+      { label: 'Deutsch', code: 'de', available: false},
+      { label: 'English', code: 'en', available: true},
+      { label: 'Español', code: 'es', available: true},
+      { label: 'Euskara', code: 'eu', available: true},
+      { label: 'Français', code: 'fr', available: false},
+      { label: 'Galego', code: 'gl', available: false},
+      { label: 'Italiano', code: 'it', available: false},
+      { label: 'Japanese', code: 'ja', available: false},
+      { label: 'Português', code: 'pt', available: false},
+      { label: 'Pусский', code: 'ru', available: false}
     ];
 
 export const themesListSelection = [
-  {id: "coral", code: "#ff7f50", name: "Coral"},
-  {id: "light", code: "#f4f4f4", name: "Light"},
-  {id: "lightseagreen", code: "#200b2aa", name: "Servirace"},
-  {id: "twitter", code: "#55acee", name: "Twitter"},
-  {id: "danger", code: "#f53d3d", name: "Danger"},
-  {id: "violet", code: "#ee82ee", name: "Violet"}
+  {id: "coral", code: "#ff7f50", name: "Coral", available: true},
+  {id: "light", code: "#f4f4f4", name: "Light", available: false},
+  {id: "lightseagreen", code: "#200b2aa", name: "Servirace", available: true},
+  {id: "twitter", code: "#55acee", name: "Twitter", available: true},
+  {id: "danger", code: "#f53d3d", name: "Danger", available: true},
+  {id: "violet", code: "#ee82ee", name: "Violet", available: true}
 ];
 
 export const convertionsList = [
